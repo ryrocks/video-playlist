656adffbc5f94889bef911ec6aeb7a97d487850e
Raymond Yeh <raymond.yeh@milkfish.digital>
2023-04-30 10:38:31 +1000

    feat: integrate playlists and videos to coressponding routes

--- src/routes/playlists.tsx
+++ src/routes/playlists.tsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from 'react';
+import { PlaylistItem } from '../components/playlist-item';
+import { Playlist } from '../interfaces/playlist';
+
 export function Playlists() {
+  const [playlists, setPlaylists] = useState<Playlist[]>([]);
+
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await fetch('/playlists.json');
+        const data = await response.json();
+        setPlaylists(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        // console.log('finally');
+      }
+    }
+
+    fetchVideos();
+  }, []);
+
   return (
     <main>
       <h1>Playlists route</h1>
+      {playlists.map((playlist) => (
+        <PlaylistItem key={playlist.id} playlist={playlist} />
+      ))}
     </main>
   );
 }
--- src/routes/videos.tsx
+++ src/routes/videos.tsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from 'react';
+import VideoItem from '../components/video-item';
+import { Video } from '../interfaces/video';
+
 export function Videos() {
+  const [videos, setVideos] = useState<Video[]>([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await fetch('/videos.json');
+        const data = await response.json();
+        setVideos(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        // console.log('finally');
+      }
+    }
+
+    fetchVideos();
+  }, []);
+
   return (
     <main>
       <h1>Videos route</h1>
+      {videos.map((video) => (
+        <VideoItem key={video.id} video={video} />
+      ))}
     </main>
   )
 }
