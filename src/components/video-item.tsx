10a37f4d20639f9de7ce1020b072ed42744edd4b
Raymond Yeh <raymond.yeh@milkfish.digital>
2023-04-30 11:45:02 +1000

    feat(comp): add videos to the selected playlist

--- src/components/video-item.tsx
+++ src/components/video-item.tsx
@@ -1,12 +1,24 @@
-import { Col, Image, Row } from 'react-bootstrap';
+import { Col, Image, Row, Button, Dropdown, DropdownButton } from 'react-bootstrap';
 import { Video } from '../interfaces/video';
+import { Playlist } from '../interfaces/playlist';
 
 interface VideoItemProps {
   video: Video;
+  playlists: Playlist[];
+  onAdd: (playlistId: number, video: Video) => void;
+  onRemove: (playlistId: number, video: Video) => void;
 }
 
 export default function VideoItem(props: VideoItemProps) {
-  const { video } = props;
+  const { video, playlists, onAdd, onRemove } = props;
+
+  const handleAddToPlaylist = (playlistId: number) => {
+    onAdd(playlistId, video);
+  };
+
+  const handleRemoveFromPlaylist = (playlistId: number) => {
+    onRemove(playlistId, video);
+  };
 
   return (
     <Row>
@@ -16,6 +28,20 @@ export default function VideoItem(props: VideoItemProps) {
       <Col xs='12' md='9' className='mb-3'>
         <h2 className='h4'>{video.name}</h2>
         <p>{video.description}</p>
+        <DropdownButton id={`video-${video.id}-dropdown`} title="Add to playlist">
+          {playlists.map((playlist) => (
+            <Dropdown.Item key={playlist.id} onClick={() => handleAddToPlaylist(playlist.id)}>
+              {playlist.name}
+            </Dropdown.Item>
+          ))}
+        </DropdownButton>
+        <DropdownButton id={`video-${video.id}-dropdown-remove`} title="Remove from playlist">
+          {playlists.map((playlist) => (
+            <Dropdown.Item key={playlist.id} onClick={() => handleRemoveFromPlaylist(playlist.id)}>
+              {playlist.name}
+            </Dropdown.Item>
+          ))}
+        </DropdownButton>
       </Col>
     </Row>
   )
--- src/routes/videos.tsx
+++ src/routes/videos.tsx
@@ -1,9 +1,31 @@
 import { useState, useEffect } from 'react';
 import VideoItem from '../components/video-item';
 import { Video } from '../interfaces/video';
+import { useFetchPlaylists } from '../hooks/useFetchPlaylists';
 
 export function Videos() {
   const [videos, setVideos] = useState<Video[]>([]);
+  const { playlists, handleSetPlaylists } = useFetchPlaylists();
+
+  const handleAddVideoToPlaylist = (playlistId: number, video: Video) => {
+    const updatedPlaylists = playlists.map((playlist) => {
+      if (playlist.id === playlistId && !playlist.videoIds.includes(video.id)) {
+        return { ...playlist, videoIds: [...playlist.videoIds, video.id] };
+      }
+      return playlist;
+    });
+    handleSetPlaylists(updatedPlaylists);
+  };
+
+  const handleRemoveVideoFromPlaylist = (playlistId: number, video: Video) => {
+    const updatedPlaylists = playlists.map((playlist) => {
+      if (playlist.id === playlistId && playlist.videoIds.includes(video.id)) {
+        return { ...playlist, videoIds: playlist.videoIds.filter((id) => id !== video.id) };
+      }
+      return playlist;
+    });
+    handleSetPlaylists(updatedPlaylists);
+  };
 
   useEffect(() => {
     const fetchVideos = async () => {
@@ -25,7 +47,8 @@ export function Videos() {
     <main>
       <h1>Videos route</h1>
       {videos.map((video) => (
-        <VideoItem key={video.id} video={video} />
+        <VideoItem key={video.id} video={video} playlists={playlists} onAdd={handleAddVideoToPlaylist}
+          onRemove={handleRemoveVideoFromPlaylist} />
       ))}
     </main>
   )
